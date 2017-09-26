7fb17d5bb5a424f341044ee3aa090a8358f95d3f
Joshua Lau <jerichowalls7@gmail.com>
2017-09-27 09:12:38 +1000

    Runs code according to level
    Logs each state to the output.

--- ts/src/Main.ts
+++ ts/src/Main.ts
@@ -1,9 +1,16 @@
 namespace Main {
 
 export function runGame(grid: World.Grid, code: String) {
-    return Parser.parseAST([], [], code);
-    // TODO: return the game with the returned AST, passing it to the
-    // interpreter.
+    let ast = Parser.parseAST([], [], code).ast;
+    console.log(ast);
+    console.log(grid);
+    let interpreterGen = Interpreter.runInterpreter(grid, ast, [
+        Purescript.Interpreter.environment.moveLeft]);
+    for (let curGrid of interpreterGen) {
+        console.log(curGrid.playerFacing());
+        console.log(curGrid.playerLocation);
+        console.log(curGrid.victory());
+    }
 }
 
 }
