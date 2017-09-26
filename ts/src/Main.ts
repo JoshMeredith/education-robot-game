b28216047103ba6a7cebb6b5d47af434110b5120
Josh Meredith <joshmeredith2008@gmail.com>
2017-09-26 18:59:32 +1000

    Fix typescript parser type signature

--- ts/src/Main.ts
+++ ts/src/Main.ts
@@ -1,7 +1,7 @@
 namespace Main {
 
 export function runGame(grid: World.Grid, code: String) {
-    return Parser.parseAST([], null as Purescript.Types.Environment, code);
+    return Parser.parseAST([], [], code);
     // TODO: return the game with the returned AST, passing it to the
     // interpreter.
 }
