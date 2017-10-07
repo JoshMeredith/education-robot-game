e32e800d7315deecf716a00d03cf51378f6fd154
capuanomat <capuanomat@gmail.com>
2017-10-08 00:45:13 +1100

    Created challenges page, modified app.py to go there

--- web/app.py
+++ web/app.py
@@ -17,3 +17,7 @@ WORLDS = {
 @app.route('/<level>')
 def hello_world(level):
     return render_template('index.html', grid=WORLDS[level])
+
+@app.route('/challenges')
+def hello_world_2():
+    return render_template('challenges.html')
