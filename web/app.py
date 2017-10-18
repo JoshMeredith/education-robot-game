08b4ca02ec31f33591eec3d68c2825e684196bf9
Joshua Lau <jerichowalls7@gmail.com>
2017-10-18 16:39:10 +1100

    Restructure templates
    Refactored common elements to a separate layout.html. Made a separate
    (skeleton) index page pre-empting the proper landing page. Cleared up
    un-needed build.js script when reaching all pages except the levels.
    Changed static URI's to be absolute - this is essential so endpoint
    routes can be changed at whim. Changed URI endpoints to things that
    make more sense. Updated URI's to be flask-like.

--- web/app.py
+++ web/app.py
@@ -5,12 +5,17 @@ from levels import LEVELS, WORLDS
 
 app = Flask(__name__, static_url_path='/static')
 
-@app.route('/<level>')
-def serve_level(level):
-    return render_template('index.html', grid=LEVELS[level])
-
-@app.route('/levels')
 @app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/<level>')
+@app.route('/<level>/')
+def serve_level(level):
+    return render_template('level.html', level=LEVELS[level])
+
+@app.route('/challenges')
+@app.route('/challenges/')
 def level_selector():
     world_levels = [
         [ {'tag': k, 'name': LEVELS[k]["name"], 'skin': LEVELS[k]["skin"]}
